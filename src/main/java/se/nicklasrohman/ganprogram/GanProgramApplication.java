b68f76c7fc0925de0a5ab73998b84dff25074ded
NicklasRohman <nicklasrohman@gmail.com>
2022-04-08 21:30:00 +0200

    add spring boot

---
+++ src/main/java/se/nicklasrohman/ganprogram/GanProgramApplication.java
@@ -0,0 +1,13 @@
+package se.nicklasrohman.ganprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/nicklasrohman/ganprogram/GanProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nicklasrohman.ganprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
